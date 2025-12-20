fe37707ebd62520da27af44b949d589b5d6b15e8
devildudez <zcoder2005@gmail.com>
2025-12-20 14:26:10 +0300

    Create homework.py

---
+++ homework.py
@@ -0,0 +1,40 @@
+"""""
+def a(x):
+    small: int = -20340134
+    big: int = 203059
+    pos: int = 0
+    pos1: int = 0
+    for i in range(len(x)):
+        if x[i] < small:
+            pos=i
+        elif x[i] > big:
+            pos1 = i
+        if x[i] == big or x[i] == small:
+            del x[i]
+            continue
+
+f=[int(i) for i in input().split()]
+print(a(f))
+"""""
+"""""
+a:list=[i for i in input().split()]
+b:list=[]
+for i in range(len(a)):
+    if a[i] not in b:
+        b.append(a[i])
+for i in range(len(b)):
+    print(b[i])
+
+"""""
+
+a:list=[int(i) for i in input().split()]
+b:list=[]
+for i in range(len(a)):
+    if a[i]<0:
+        b.append(a[i])
+    elif a[i]>=0:
+       b.append(a[i])
+    elif a[i]==0:
+        b.append(a[i])
+print(b)
+
