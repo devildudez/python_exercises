fcbd0f1ab713f667751845af39fdda981c149e20
jojoprison <egyabig2@gmail.com>
2025-12-28 19:16:31 +0700

    dz check

--- hw_26.12/code_war_array_sum.py
+++ hw_26.12/code_war_array_sum.py
@@ -1,3 +1,11 @@
+task = '''
+I'm new to coding and now I want to get the sum of two arrays... 
+Actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+'''
+
+
 def array_plus_array(arr1, arr2) -> int:
     sm1 = 0
     sm2 = 0
