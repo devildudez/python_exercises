095a505039ca5cc28a2a0a27a4dfa1d425f124cf
jojoprison <egyabig2@gmail.com>
2026-01-02 20:16:17 +0700

    решение 1498

--- lesson_7_02_01_26/1498_min_moves_arr_elems_eq.py
+++ lesson_7_02_01_26/1498_min_moves_arr_elems_eq.py
@@ -4,5 +4,27 @@ In one operation, you can increase any element by 1.
 Find min operations to make all elements equal
 '''
 
+
+def min_moves(arr: list) -> int:
+
+    if not arr:
+        return 0
+
+    # кол во действий (moves)
+    x: int = 0
+    max_val = max(arr)
+
+    for i in range(len(arr)):
+
+        while arr[i] != max_val:
+            arr[i] += 1
+            x += 1
+
+    return x
+
+
 if __name__ == '__main__':
-    pass
+    # lst = [1, 2, 3]
+    lst = []
+    res = min_moves(lst)
+    print(res)
